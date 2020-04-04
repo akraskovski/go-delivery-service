97a777f1a69c8b0e715fdd9e35d32dac71ee7f6d
akraskovski <artem.kraskovski@gmail.com>
2020-04-04 14:31:29 +0300

    add a mux router with simple hello route

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/gorilla/mux v1.7.4
 	github.com/sirupsen/logrus v1.5.0
 )
--- internal/app/delivery_service/api_server.go
+++ internal/app/delivery_service/api_server.go
@@ -1,16 +1,23 @@
 package delivery_service
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+)
 
 type APIServer struct {
 	config *DeliveryServiceConfig
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 func NewAPIServer(config *DeliveryServiceConfig) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -20,8 +27,9 @@ func (server *APIServer) Start() error {
 	}
 
 	server.logger.Info("Starting the API Server")
+	server.configureRouter()
 
-	return nil
+	return http.ListenAndServe(server.config.BindPort, server.router)
 }
 
 func (server *APIServer) configureLogger() (err error) {
@@ -32,4 +40,14 @@ func (server *APIServer) configureLogger() (err error) {
 
 	server.logger.SetLevel(level)
 	return nil
+}
+
+func (server *APIServer) configureRouter() {
+	server.router.HandleFunc("/hello", handleHello())
+}
+
+func handleHello() http.HandlerFunc {
+	return func(responseWriter http.ResponseWriter, request *http.Request) {
+		io.WriteString(responseWriter, "Welcome to the club, buddy\n")
+	}
 }
