67c51afc4ed861513c1b6ecbc04b83ea866b9587
akraskovski <artem.kraskovski@gmail.com>
2020-04-05 17:26:22 +0300

    order repository methods were implemented

--- internal/app/store/repository/mongo/order_repository.go
+++ internal/app/store/repository/mongo/order_repository.go
@@ -1,17 +1,47 @@
 package mongo
 
 import (
+	"context"
 	"github.com/akraskovski/go-delivery-service/internal/app/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
 )
+
+const collectionName string = "orders"
 
 type OrderRepository struct {
 	store *Store
 }
 
 func (repository *OrderRepository) Create(order *model.Order) (*model.Order, error) {
-	return nil, nil
+	if _, err := repository.store.db.Collection(collectionName).InsertOne(context.Background(), order); err != nil {
+		log.Fatal(err)
+	}
+	return order, nil
 }
 
 func (repository *OrderRepository) FindAll() ([]*model.Order, error) {
-	return []*model.Order{}, nil
+	var results []*model.Order
+	cursor, err := repository.store.db.Collection(collectionName).Find(context.Background(), bson.D{})
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(context.Background())
+	for cursor.Next(context.Background()) {
+
+		// create a value into which the single document can be decoded
+		var elem model.Order
+		err := cursor.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, &elem)
+	}
+
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return results, nil
 }
